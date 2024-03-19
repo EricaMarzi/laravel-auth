0d6439fb03f34baea5d1ec2d3a45c51464dbc002
EricaMarzi <erica.marzi.1993@gmail.com>
2024-03-19 19:07:46 +0100

    table add

--- database/migrations/2024_03_19_171126_create_posts_table.php
+++ database/migrations/2024_03_19_171126_create_posts_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->string('image')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
